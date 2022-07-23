2b4c527d53dab889c36d91fd9f3875425c37c336
Digital Nomad <90147636+derkiner@users.noreply.github.com>
2022-07-23 10:01:12 +0300

    Update BirthdayCakeCandles.js

--- BirthdayCakeCandles.js
+++ BirthdayCakeCandles.js
@@ -1,9 +1,7 @@
 // Problem => https://www.hackerrank.com/challenges/birthday-cake-candles/problem 
 /* You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
 
-Example
-
-
+Example:
 The maximum height candles are  units high. There are  of them, so return .
 
 Function Description
