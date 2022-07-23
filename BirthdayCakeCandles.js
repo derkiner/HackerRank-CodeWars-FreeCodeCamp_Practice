4fa487985d69461c3c558fe265b589a60cf9682c
Digital Nomad <90147636+derkiner@users.noreply.github.com>
2022-07-23 09:53:56 +0300

    HackerRank Added

---
+++ BirthdayCakeCandles.js
@@ -0,0 +1,9 @@
+// Problem => https://www.hackerrank.com/challenges/birthday-cake-candles/problem 
+
+
+function birthdayCakeCandles(n, ar) {
+    ar.sort(function(a,b){
+        return b-a;
+    });
+    var count=1
+}
