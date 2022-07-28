a45bb0d5efe0324bac06e4a27f1fa5cf568e86ff
Digital Nomad <90147636+derkiner@users.noreply.github.com>
2022-07-28 10:50:08 +0300

    Create ConfirmingTheEnding.js

---
+++ ConfirmingTheEnding.js
@@ -0,0 +1,3 @@
+// Problem => https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/confirm-the-ending
+
+const confirmEnding = (str, target) => str.substring(str.length-target.length)===target;
