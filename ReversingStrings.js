0b0e1bd9d4efaed148b750fbfe20bef4b4be9fca
Digital Nomad <90147636+derkiner@users.noreply.github.com>
2022-07-28 10:47:38 +0300

    Create ReversingStrings.js

---
+++ ReversingStrings.js
@@ -0,0 +1,4 @@
+// Problem => https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/reverse-a-string
+
+
+const reverseString = str => str.split("").reverse().join("");
