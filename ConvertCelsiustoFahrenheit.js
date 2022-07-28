9ec260d55750c5796b4af558dbdaaf89acf177e1
Digital Nomad <90147636+derkiner@users.noreply.github.com>
2022-07-28 10:45:23 +0300

    Create ConvertCelsiustoFahrenheit.js

---
+++ ConvertCelsiustoFahrenheit.js
@@ -0,0 +1,8 @@
+// Problem => https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/convert-celsius-to-fahrenheit
+
+function convert(celsius) {
+  let fahrenheit=(9/5)*celsius+32;
+  return fahrenheit;
+}
+
+convert(20);
