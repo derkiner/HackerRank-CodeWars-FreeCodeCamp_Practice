584965fe627ab7325d41e9be149d936a2dc073b8
Digital Nomad <90147636+derkiner@users.noreply.github.com>
2022-07-28 10:30:48 +0300

    Create MatchNumbers+LettersOfTheAlphabeth.js

---
+++ MatchNumbers+LettersOfTheAlphabeth.js
@@ -0,0 +1,6 @@
+
+// PROBLEM => https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/match-numbers-and-letters-of-the-alphabet
+
+let quoteSample = "Blueberry 3.1415922653.";
+let myRegex = /[h-s2-6]/ig;
+let result = quoteSample.match(myRegex);
