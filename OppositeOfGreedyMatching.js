35bf2e8cd8049349283a2efe68875cca945101d3
Digital Nomad <90147636+derkiner@users.noreply.github.com>
2022-07-28 10:35:22 +0300

    Create OppositeOfGreedyMatching.js

---
+++ OppositeOfGreedyMatching.js
@@ -0,0 +1,6 @@
+
+// PROBLEM => https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/find-characters-with-lazy-matching
+
+let text = "<h1>Winter is coming</h1>
+let myRegex = /<h.*?1>/;
+let result = text.match(myRegex);
