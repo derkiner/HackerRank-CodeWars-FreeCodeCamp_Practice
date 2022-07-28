e41f08e584de0d9d1c0b1fe3ac12dfe22abb182e
Digital Nomad <90147636+derkiner@users.noreply.github.com>
2022-07-28 10:11:25 +0300

    Create MatchLiteralStrings.js

---
+++ MatchLiteralStrings.js
@@ -0,0 +1,6 @@
+// PROBLEM => https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/match-literal-strings
+
+
+let waldoIsHiding = "Somewhere is hiding in this text.";
+let waldoRegex = /Waldo/;
+let result = waldoRegex.test(waldoIsHiding);
