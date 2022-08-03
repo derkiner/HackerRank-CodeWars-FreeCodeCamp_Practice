0b65f96b5d38e672ebf8c6a8f1cc90458dfa5f1a
Digital Nomad <90147636+derkiner@users.noreply.github.com>
2022-08-03 14:51:12 +0300

    Create Hoisting.js

---
+++ Hoisting.js
@@ -0,0 +1,17 @@
+// What will be console.logged here
+//
+// Question 1
+// console.log(foo);
+// foo = 1;
+Error: foo is not defined
+
+// question 2
+// console.log(foo);
+// var foo = 2;
+undefined
+
+// question 3
+// foo = 3;
+// console.log(foo);
+// var foo;
+3 (Hoisted)
